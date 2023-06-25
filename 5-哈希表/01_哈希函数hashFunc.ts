a71e0ad9ee7154d85396e43691131d5d730d749f
Chiaki-xps <1490295446@qq.com>
2023-06-25 17:18:37 +0800

    feat: 哈希函数

--- 5-哈希表/01_哈希函数hashFunc.ts
+++ 5-哈希表/01_哈希函数hashFunc.ts
@@ -6,7 +6,6 @@
  */
 function hashFunc(key: string, max: number): number {
   // 1. 计算hashCode cats => 60337 (27为底的时候)
-
   let hashCode = 0;
 
   const length = key.length;
@@ -14,8 +13,21 @@ function hashFunc(key: string, max: number): number {
     // 霍纳法则计算hashCode
 
     // charCodeAt获取UTF16编码格式
+    // 31就是我们霍纳法则的X,是我们选择的幂
     hashCode = 31 * hashCode + key.charCodeAt(i);
   }
 
-  return -1;
+  // 取模运算/取余运算
+  const index = hashCode % max;
+  return index;
 }
+
+// 测试哈希函数
+console.log(hashFunc('abc', 7));
+console.log(hashFunc('cba', 7));
+console.log(hashFunc('nba', 7));
+console.log(hashFunc('mba', 7));
+
+// 需要注意的是,存放的字符串key不应该重复.重复没有啥意思。
+console.log(hashFunc('aaa', 7));
+console.log(hashFunc('bbb', 7));
