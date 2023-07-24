60b626a829b157fe05245820a9af65025e858daa
Chiaki-xps <1490295446@qq.com>
2023-07-25 00:17:43 +0800

    feat: 排序

--- 12-排序算法实现/04_归并排序(mergeSort).ts
+++ 12-排序算法实现/04_归并排序(mergeSort).ts
@@ -1,28 +1,48 @@
-import { testSort } from 'hy-algokit';
+import { measureSort, testSort } from 'hy-algokit';
 
 function mergeSort(arr: number[]): number[] {
+  // 递归结束条件
+  if (arr.length <= 1) return arr;
+
   // 1. 分解（divide): 对数组进行分解(分解成两个小数组)
+  // 1.1 切割数组
   const mid = Math.floor(arr.length / 2);
   const leftArr = arr.slice(0, mid);
   const rightArr = arr.slice(mid);
 
-  // 2. 合并(merge): 将两个子数组进行合并(双指针)
-  // 返回新数组
-  return mergeSortedArr(mergeSort(leftArr), mergeSort(rightArr));
-}
+  // 1.2 递归的切割leftArr和rightArr
+  const newLeftArr = mergeSort(leftArr);
+  const newRightArr = mergeSort(rightArr);
 
-// 合并两个有序数组
-function mergeSortedArr(a: number[], b: number[]): number[] {
-  let i = 0,
-    j = 0;
-  // 声明新数组
-  const mergeArr: number[] = [];
-  while (i < a.length && j < b.length) {
-    // 将较小的元素放入新数组
-    a[i] < b[j] ? mergeArr.push(a[i++]) : mergeArr.push(b[j++]);
+  // 2. 合并(merge): 将两个子数组进行合并(双指针)
+  // 2.1 定义双指针
+  const newArr: number[] = [];
+  let i = 0;
+  let j = 0;
+  // 左右两边必须有值又能够比较，不然指针i或j指向的值undefined
+  while (i < newLeftArr.length && j < newRightArr.length) {
+    if (newLeftArr[i] <= newRightArr[j]) {
+      newArr.push(newLeftArr[i]);
+      i++;
+    } else {
+      newArr.push(newRightArr[j]);
+      j++;
+    }
   }
 
-  return mergeArr;
+  // 2.2 判断是否某一个数组中还有剩余的元素
+  // 执行到这里的时候，存在一个数组为空，另一个数组有值的情况，这时候把有值的数组拼接到newArr最后
+  // 循环完左边还有剩余
+  if (i < newLeftArr.length) {
+    newArr.push(...newLeftArr.slice(i));
+  }
+  // 循环完右边还有剩余
+  if (j < newLeftArr.length) {
+    newArr.push(...newRightArr.slice(j));
+  }
+
+  return newArr;
 }
 
-testSort(mergeSort);
+// testSort(mergeSort);
+measureSort(mergeSort);
